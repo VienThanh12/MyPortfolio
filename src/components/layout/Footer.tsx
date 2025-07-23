311ca0b4526906b9abf59f7383b9b294552b4622
Kevin <67015555+VienThanh12@users.noreply.github.com>
2025-07-23 19:12:35 +0300

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -54,7 +54,7 @@ const Footer = () => {
               <svg className="w-5 h-5 mr-2 text-indigo-600 dark:text-indigo-400" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M2 5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2V5z" />
               </svg>
-              <span>Espoo, Finland</span>
+              <span>Helsinki, Finland</span>
             </li>
           </ul>
         </div>
